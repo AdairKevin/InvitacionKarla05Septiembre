10cbab6bc3bf4450706ead82a3012f607e9112d8
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2026-08-08 18:29:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function abrirSobre() {
   if (!sobre.classList.contains("abierto")) {
     sobre.classList.add("abierto");
     sobre.onclick = null; // Desactiva el evento de clic después de abrir
+    musica.play().catch((error) => {
+      console.log("No se pudo reproducir la música:", error);
+    });
   }
 }
 
